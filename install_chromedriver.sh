9fbf83007fb7b9d2041cb91f5d9be3b384b627eb
andrgit <andrgit1@gmail.com>
2022-02-02 22:49:40 +0100

    Update Dockerfile

--- install_chromedriver.sh
+++ install_chromedriver.sh
@@ -2,10 +2,10 @@ CHROME_VERSION='google-chrome-stable'
 CHROME_MAJOR_VERSION=$(google-chrome — version | sed -E 's/.* ([0–9]+)(\.[0–9]+){3}.*/\1/')
 CHROME_DRIVER_VERSION=$(wget — no-verbose -O — 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_MAJOR_VERSION}')
 echo 'Using chromedriver version: '$CHROME_DRIVER_VERSION
-wget — no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+wget — no-verbose -O /tmp/chromedriver_mac64.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_mac64.zip
 rm -rf /opt/selenium/chromedriver
-unzip /tmp/chromedriver_linux64.zip -d /opt/selenium
-rm /tmp/chromedriver_linux64.zip
+unzip /tmp/chromedriver_mac64.zip -d /opt/selenium
+rm /tmp/chromedriver_mac64.zip
 mv /opt/selenium/chromedriver /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION
 chmod 755 /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION
 ln -fs /opt/selenium/chromedriver-$CHROME_DRIVER_VERSION /usr/bin/chromedriver
