0e64aa797e3af8ebcfc1c1797d189dd46b7043e0
andrgit <86713245+andrgit@users.noreply.github.com>
2022-02-03 10:32:36 +0100

    Update test_panda2.py

--- test_panda2.py
+++ test_panda2.py
@@ -1,20 +1,17 @@
-# import allure
 from selenium.webdriver.common.by import By
 
 
-# from selenium.webdriver import Chrome
-
-# @allure.story("Check valid plugin for shining panda")
-# def test_jenkinks_plugin(browser):
-#     with allure.step("Open Jenkins page"):
-#         browser.get("https://plugins.jenkins.io/shiningpanda")
-#     with allure.step("Find title"):
-#         title_element = browser.find_element(By.XPATH, "//*[@class='title']")
-#     with allure.step("Assert Title"):
-#         assert title_element.text == "ShiningPanda"
-
-
+@allure.story("Check valid plugin for shining panda")
 def test_shining_panda(browser):
-    browser.get("https://plugins.jenkins.io/shiningpanda")
-    title_element = browser.find_element(By.XPATH, "//*[@class='title']")
-    assert title_element.text == "ShiningPanda"
+    with allure.step("Open Jenkins page"):
+        browser.get("https://plugins.jenkins.io/shiningpanda")
+    with allure.step("Find title"):
+        title_element = browser.find_element(By.XPATH, "//*[@class='title']")
+    with allure.step("Assert Title"):
+        assert title_element.text == "ShiningPanda"
+
+
+# def test_shining_panda(browser):
+#     browser.get("https://plugins.jenkins.io/shiningpanda")
+#     title_element = browser.find_element(By.XPATH, "//*[@class='title']")
+#     assert title_element.text == "ShiningPanda"
